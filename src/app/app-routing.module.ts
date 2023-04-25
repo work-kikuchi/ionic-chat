5752effced4232f34e93fcc0016a08109986f9dd
tkikuchi <tkikuchi@landscape.co.jp>
2023-04-25 16:31:50 +0900

    app-routingへauth追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from "@angular/fire/compat/auth-guard";
+
+const redirectUnauthorized = ()=> redirectUnauthorizedTo(['auth/signin'])
+const redirectLoggedIn = ()=>redirectLoggedInTo(['/'])
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate:[AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorized }
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate:[AngularFireAuthGuard],
+    data:{ authGuardPipe: redirectLoggedIn }
   }
 ];
 @NgModule({
