97091ef281a8a10f30598c238a98565c44d3add9
tkikuchi <tkikuchi@landscape.co.jp>
2023-04-25 12:35:56 +0900

    moduleまとめ

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { IonicModule } from "@ionic/angular";
+import { SigninPage } from "./signin/signin.page";
+import { SignupPage } from "./signup/signup.page";
+
+const routes: Routes = [
+  {
+    path:'signup',
+    component:SignupPage
+  },
+  {
+    path:'signin',
+    component:SigninPage
+  }
+]
+
+@NgModule({
+  declarations: [SigninPage,SignupPage],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthModule { }
