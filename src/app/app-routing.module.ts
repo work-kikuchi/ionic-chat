52613ad2add13a5a1e3cca3d27a33321fd6acd74
tkikuchi <tkikuchi@landscape.co.jp>
2023-04-25 11:59:36 +0900

    signin/signupのルーティング追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,14 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./auth/signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./auth/signin/signin.module').then( m => m.SigninPageModule)
   }
 ];
 @NgModule({
