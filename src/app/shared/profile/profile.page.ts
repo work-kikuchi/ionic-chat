21a0dfbdab3503f6fcd54becb01c54e4c6305007
tkikuchi <tkikuchi@landscape.co.jp>
2023-04-26 11:41:27 +0900

    プロフィールページの作成

---
+++ src/app/shared/profile/profile.page.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { AuthService } from "../../auth/auth.service";
+import { FirestoreService, IUser } from "../firestore.service";
+import { ViewWillEnter } from "@ionic/angular";
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+})
+export class ProfilePage implements OnInit,ViewWillEnter {
+  uid:string='';
+  user:IUser = {
+    displayName: '',
+    photoDataUrl: ''
+  };
+  photo:string | undefined='';
+
+  constructor(
+    private modalController: ModalController,
+    private authService: AuthService,
+    private fireStore: FirestoreService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  async ionViewWillEnter():Promise<void>{
+    this.uid = await this.authService.getUserId()
+    const user = await this.fireStore.userInit(this.uid)
+    if (user){
+      this.user = user
+    }
+  }
+
+  async updateProfile():Promise<void>{
+    if(this.photo){
+      this.user.photoDataUrl = this.photo
+    }
+    await this.fireStore.userSet(this.user);
+    await this.modalController.dismiss()
+  }
+
+  async modalDismiss():Promise<void>{
+    await this.modalController.dismiss();
+  }
+
+  async takePicture():Promise<void>{
+    const image = await Camera.getPhoto({
+      quality:100,
+      resultType: CameraResultType.DataUrl
+    })
+    this.photo = image && image.dataUrl
+  }
+
+}
