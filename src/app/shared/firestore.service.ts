c7e874b5bd2340e57cc9c819dcdbb1d812217160
tkikuchi <tkikuchi@landscape.co.jp>
2023-04-26 14:56:58 +0900

    チャットアプリ完成

--- src/app/shared/firestore.service.ts
+++ src/app/shared/firestore.service.ts
@@ -1,20 +1,57 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreDocument } from "@angular/fire/compat/firestore";
-import { first } from "rxjs/operators";
-import { lastValueFrom } from "rxjs";
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection,
+  DocumentReference
+} from "@angular/fire/compat/firestore";
+import { first, concatMap } from "rxjs/operators";
+import { lastValueFrom, Observable, firstValueFrom } from "rxjs";
 
 export interface IUser{
   displayName:string,
   photoDataUrl:string
 }
+
+export interface IMessage{
+  uid:string,
+  message:string,
+  timestamp:number
+}
+
+export interface IChat extends IUser, IMessage{}
+
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
   userDoc: AngularFirestoreDocument<IUser> | undefined;
+  messageCollection: AngularFirestoreCollection<IMessage>;
+  userCollection: AngularFirestoreCollection<IUser>;
+
   constructor(
     private fireStore: AngularFirestore
-  ) { }
+  ) {
+    this.messageCollection = this.fireStore.collection<IMessage>('chat', ref=>ref.orderBy('timestamp','desc'));
+    this.userCollection = this.fireStore.collection<IUser>('users');
+  }
+
+  messageAdd(message:IMessage):Promise<DocumentReference<IMessage>>{
+    return this.messageCollection.add(message)
+  }
+
+  chatInit():Observable<IChat[]>{
+    return this.messageCollection.valueChanges({idField:'messageId'}).pipe(
+      concatMap(async message=>{
+        const users = await firstValueFrom(this.userCollection.valueChanges({idField:'uid'}).pipe(
+          first()));
+         return message.map(message=>{
+           const user = users?.find(u => u.uid === message.uid);
+           return Object.assign(message, user)
+         })
+      })
+    )
+  }
 
   userInit(uid:string):Promise<IUser | undefined>{
     this.userDoc = this.fireStore.doc<IUser>('users/'+uid);
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController, IonContent, ViewWillEnter } from '@ionic/angular';
 import { ProfilePage } from '../shared/profile/profile.page';
+import { AuthService } from "../auth/auth.service";
+import { FirestoreService, IChat, IUser } from "../shared/firestore.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit, ViewWillEnter{
+  message = '';
+  // @ts-ignore
+  uid:string;
+  // @ts-ignore
+  user:IUser;
+  // @ts-ignore
+  chat: Observable<IChat[]>;
+  
+  @ViewChild(IonContent, {static:true})// @ts-ignore
+  content: IonContent;
 
   constructor(
-    private modalController:ModalController
+    private modalController:ModalController,
+    private auth:AuthService,
+    private fireStore:FirestoreService
   ) {}
 
   async ngOnInit(): Promise<void> {
-    const modal = await this.modalController.create({
-      component: ProfilePage
+    const user = await this.fireStore.userInit(await this.auth.getUserId())
+    if(!user) {
+      const modal = await this.modalController.create({
+        component: ProfilePage
+      });
+      await modal.present()
+      modal.onWillDismiss().then(()=>{
+        this.ionViewWillEnter()
+      });
+    }
+    this.chat = this.fireStore.chatInit()
+  }
+
+  async ionViewWillEnter():Promise<void> {
+    this.uid = await this.auth.getUserId();
+    // @ts-ignore
+    this.user = await this.fireStore.userInit(this.uid)
+  }
+
+  async postMessage(){
+    if(!this.user){
+      alert('プロフィールの登録が必要です')
+      return;
+    }
+    await this.fireStore.messageAdd({
+      uid: this.uid,
+      message: this.message,
+      timestamp: Date.now()
     });
-    await modal.present()
+    this.message = ''
+    await this.content.scrollToTop(100);
+  }
+
+  trackByFn(index:any, item:any){
+    return item.messageId;
   }
 
 }
